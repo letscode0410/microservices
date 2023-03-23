c857f3f793f0665edf7500eb22edcdc9228bfe57
Ravi Kumar P <ravikumar.p536@gmail.com>
2023-03-23 19:13:37 +0530

    implemented handler for GetAllBrands

---
+++ EShopping/Catalog/Catalog.Application/Mappers/ProductMapperProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Mappers;
+
+public class ProductMapperProfile:Profile
+{
+    public ProductMapperProfile()
+    {
+        CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+    }
+}

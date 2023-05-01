ac2a38893c6d58048b3c2653735624f524c99f0e
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-01 12:39:55 +0530

    grpc implementation

--- EShopping/Discount/Discount.API/Program.cs
+++ EShopping/Discount/Discount.API/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Discount.API;
+using Discount.Infrastructure.Extensions;
 
-app.Run();
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        host.MigrateDatabase<Program>();
+        host.Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
